424500808af8f5cba6d8980d5c114a54d334c974
barabanoveugene <barabanoveugene@gmail.com>
2023-01-26 11:23:18 +0200

    add chromatic

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,17 +1,20 @@
 module.exports = {
-  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  staticDirs: ["../public"],
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-    "@storybook/preset-create-react-app",
+   stories: [
+    '../src/components/**/*.stories.js'
   ],
-  framework: "@storybook/react",
-  core: {
-    builder: "@storybook/builder-webpack5",
-  },
-  features: {
-    interactionsDebugger: true,
-  },
-};
+    staticDirs: ['../public'],
+    addons: [
+      '@storybook/addon-links',
+      '@storybook/addon-essentials',
+      '@storybook/preset-create-react-app',
+      '@storybook/addon-interactions',
+      '@storybook/addon-a11y',
+    ],
+    framework: '@storybook/react',
+    core: {
+      builder: '@storybook/builder-webpack5',
+    },
+    features: {
+      interactionsDebugger: true,
+    },
+  };
